9490411e040f9c47dec837d5796fa06d3318b536
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-05-26 16:08:02 +0530

    first ever app

---
+++ app/src/main/java/com/example/round3/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.round3
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private val greet = "I love you baby !!!!"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClick(view: View) {
+        val greetTxt: TextView = findViewById(R.id.greetTxt)
+        if (greetTxt.text == greet) {
+            greetTxt.text = ""
+        } else {
+            greetTxt.text = greet
+        }
+    }
+}
